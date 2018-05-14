012edf26b7892c28288e7fdd956063a618f812a1
LamaR <LamaR@192.168.0.5>
2018-05-14 19:12:08 +0200

    Homework

--- OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -7,15 +7,50 @@ package pl.edu.ur.oopl9;
 
 /**
  *
- * @author maarchyl
+ * @author LamaR
  */
+import java.util.HashMap;
+import java.util.Scanner;
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+     public static void main(String[] args) {
+        HashMap<String, String> slowa = new HashMap<>();
+        Scanner skaner = new Scanner(System.in);
+        slowa.put("dom", "house");
+        slowa.put("mleko", "milk");
+        slowa.put("kamien", "rock");
+        slowa.put("krzak", "bush");
+        slowa.put("szklanka", "glass");
+        slowa.put("puszka", "can");
+        slowa.put("zespol", "band");
+        slowa.put("komputer", "computer");
+        slowa.put("telewizor", "television");
+        slowa.put("swieczka", "candle");
+        slowa.put("dywan", "carpet");
+        slowa.put("piwo", "beer");
+        slowa.put("impreza", "party");
+        slowa.put("koncert", "gig");
+        slowa.put("telefon", "telephone");
+        slowa.put("woda", "water");
+        slowa.put("rzecz", "thing");
+        slowa.put("eutanazja", "euthanasia");
+        slowa.put("bol", "pain");
+        slowa.put("szczescie", "happiness");
+         System.out.println("Podaj slowo ktore chcesz przetlumaczyc z ");
+        while (true) {
+            String x = skaner.next();
+            if ("koniec!".equals(x)) {
+                break;
+            } else {
+                System.out.println("W jezyku angielskim brzmi :"+slowa.get(x));
+            }
+
+        }
+
     }
+
     
 }
